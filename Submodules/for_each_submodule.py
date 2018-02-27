e77d755e91925014b976c6a151986d0b48c8e87f
Konstantin Lazukin <konstantin.lazukin@gmail.com>
2018-02-28 01:30:30 +0200

    Fix solution and submodules

--- Submodules/for_each_submodule.py
+++ Submodules/for_each_submodule.py
@@ -6,15 +6,22 @@ def run_git_command(command, repo='.') :
     git_query = subprocess.Popen(command, shell=True, cwd=repo, stdin=subprocess.PIPE, stderr=subprocess.PIPE, env=os.environ)
     (_, error) = git_query.communicate()
     res = git_query.poll()
-    if(res != 0) :
-        print(error)
+    print(error)
     return res
+
+def is_git_repo(entry):
+    if entry.is_dir():
+        with os.scandir(entry) as it:
+            for subentry in it:
+                if subentry.name == ".git":
+                    return True
+    return False
 
 def get_submodules():
     res = []
     with os.scandir('.') as it:
         for entry in it:
-            if entry.is_dir():
+            if is_git_repo(entry):
                 res.append(entry)
     return res
 
