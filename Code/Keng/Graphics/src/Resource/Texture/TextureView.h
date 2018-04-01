339287bd5c2c7d0447c58204f245fe735a9df448
Konstantin Lazukin <konstantin.lazukin@gmail.com>
2018-04-01 17:56:09 +0300

    Remove ITextureView interface

--- Code/Keng/Graphics/src/Resource/Texture/TextureView.h
+++ Code/Keng/Graphics/src/Resource/Texture/TextureView.h
@@ -8,33 +8,12 @@ namespace keng::graphics
 {
     using d3d_tools::ResourceViewType;
 
-    class ITextureView : public core::IRefCountObject
-    {
-    public:
-        virtual void* GetNativeInterface() const = 0;
-        virtual FragmentFormat GetFormat() const = 0;
-        virtual ResourceViewType GetViewType() const = 0;
-        virtual ~ITextureView() = default;
-    };
-
     template<ResourceViewType type>
     class TextureView :
-        public core::RefCountImpl<ITextureView>,
+        public core::RefCountImpl<core::IRefCountObject>,
         public d3d_tools::TextureView<type>
     {
     public:
         using d3d_tools::TextureView<type>::TextureView;
-
-        ResourceViewType GetViewType() const override {
-            return type;
-        }
-
-        void* GetNativeInterface() const override {
-            return this->GetView();
-        }
-
-        FragmentFormat GetFormat() const override {
-            return static_cast<FragmentFormat>(this->GetViewFormat());
-        }
     };
 }
